b0f82f14bc9bf8ec80c5a0da8a19bed9a2245705
dusanivkovic <ivdule77@gmail.com>
2024-04-14 15:50:48 +0200

    Add readme.md

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . './../app/views/header.php';
+
+require_once __DIR__ . './../app/views/login.php';
+
+require_once __DIR__ . './../app/views/footer.php';
+
---
+++ public/register-page.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . './../app/views/header.php';
+
+require_once __DIR__ . './../app/views/register.php';
+
+require_once __DIR__ . './../app/views/footer.php';
