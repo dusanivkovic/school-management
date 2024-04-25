0dd0b2f3f4516d8b393185fd327258fbaa5f223d
https://github.com/dusanivkovic <ivdule77@gmail.com>
2024-04-25 17:26:17 +0200

    Add full name verification

--- app/views/register.php
+++ app/views/register.php
@@ -1,8 +1,41 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+use app\models\{Db, RegisterModel};
+use app\controllers\SignUpControler;
 
 
+
+if (isset($_POST['submit']))
+{
+    $fullName = $_POST['fullname'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $classTeacher = $_POST['classteacher'];
+
+
+    //Instantiate SignUpController class
+    $signup = new SignUpControler($_POST);
+        //Grabbing the data
+    $signup->rm->loadData($_POST);
+    $signup->signUpUser();
+
+    $hasErrorUserName = $signup->rm->hasError('fullname');
+    var_dump($hasErrorUserName);
+    //Running error handlers and user signup
+    if ($signup->rm->hasError('fullname'))
+    {
+        RegisterModel::prntR($signup);
+        header('location: ../../httpdocs/index.php?page=register');
+        exit;
+    }
+
+    header('location: ../../httpdocs/index.php?error=none');
+
+    //going back to front page
+}
 ?>
-<form class="pt-3" method="post" action="../app/views/signUpView.php">
+<form class="pt-3" method="post" action="../app/views/register.php">
   <div class="form-group">
     <label>Ime i prezime</label>
     <div class="input-group">
@@ -11,7 +44,7 @@
           <i class="ti-user text-primary"></i>
         </span>
       </div>
-      <input name="fullname" type="text" class="form-control form-control-lg border-left-0" placeholder="Ime i prezime">
+      <input name="fullname" type="text" class="form-control form-control-lg border-left-0 <?php echo $hasErrorUserName ?? 'is-invalid'; ?>" placeholder="Ime i prezime" value="<?php ##echo $hasErrorUserName ?? 'Ime i prezime'?>">
     </div>
   </div>
   <div class="form-group">
