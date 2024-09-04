7808169722561b2164a33a6d46e1509fd432e346
dusanivkovic <ivdule77@gmail.com>
2024-09-04 23:05:16 +0200

    Change gitignore

--- app/views/testes/index.classes.php
+++ app/views/testes/index.classes.php
@@ -10,21 +10,22 @@ $i = 1;
 $tM = new TestModel;
 $rm = new RegisterModel;
 $testes = $tM->findTestByClass(Session::get('userId')) ?? [];
-Session::prntR(empty($testes));
+$userClass = $rm->findUserByUserId(Session::get('userId'))['class_teacher'];
 ?>
 <div class="content-wrapper">
     <div class="row">
         <div class="col-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                    <h4 class="card-title"><?= !empty($testes) ? 'odjeljenje ' . $tM->printClass($testes)  : '' ?></h4>
+                    <h4 class="card-title"><?= 'Odjeljenje ' .$userClass ?? '' ?></h4>
+                    <p class="card-description <?= $userClass ? 'd-none' : '' ?>">Izaberi razred i odjeljenje</p>
                     <p class="card-description">
-                        <form action="<?= $testes ? 'd-none' : 'dashboard.php?addClassTeacher'?>" method="POST">
+                        <form action="<?= $userClass ? 'd-none' : 'dashboard.php?addClassTeacher'?>" method="POST">
                             <input type="hidden" name="_method" value="PUT">
                             <div class="form-group row">
-                                <label class="<?= $testes ? 'd-none' : ''?>">Razred</label>
+                                <label class="<?= $userClass ? 'd-none' : ''?>">Razred</label>
                                 <div class="col-md-4">
-                                    <select name="class" class="form-control form-control-lg <?= $testes ? 'd-none' : ''?>" id="schoolclass">
+                                    <select name="class" class="form-control form-control-lg <?= $userClass ? 'd-none' : ''?>" id="schoolclass">
                                         <?php foreach ($classes as $item) : ?>
                                                 <option value="<?= $item != 'Разред' ? $item : '' ?>">
                                                     <?php echo $item ?>
@@ -33,20 +34,20 @@ Session::prntR(empty($testes));
                                     </select>
                                 </div>
                                 <div class="col-md-8 row">
-                                    <label class="<?= $testes ? 'd-none' : ''?>">Odjeljenje</label>
+                                    <label class="<?= $userClass ? 'd-none' : ''?>">Odjeljenje</label>
                                     <?php while ($i <= 5) : ?>
                                         <div class="col-2">
-                                            <input name="department[]" type="checkbox" class="form-check-input mx-1 <?= $testes ? 'd-none' : ''?>" value ="<?php echo  $i ?>" >
+                                            <input name="department[]" type="checkbox" class="form-check-input mx-1 <?= $userClass ? 'd-none' : ''?>" value ="<?php echo  $i ?>" >
                                             <?php 
-                                                echo $testes ? '' : $i; 
+                                                echo $userClass ? '' : $i; 
                                                 $i++; 
                                             ?>
                                         </div> 
                                     <?php endwhile ?>
                                 </div>
                             </div>
-                            <button type="submit" name="update-class-teacher" class="btn btn-primary me-2 <?= $testes ? 'd-none' : ''?>">Update</button>
-                            <a href="./dashboard.php?main" class="btn btn-light <?= $testes ? 'd-none' : ''?>">Cancel</a>
+                            <button type="submit" name="update-class-teacher" class="btn btn-primary me-2 <?= $userClass ? 'd-none' : ''?>">Update</button>
+                            <a href="./dashboard.php?main" class="btn btn-light <?= $userClass ? 'd-none' : ''?>">Cancel</a>
                         </form>
                     </p>
                     <?php Session::flash(); ?>
