25d924001de2b20a940227de5b316a65a8a92936
https://github.com/dusanivkovic <ivdule77@gmail.com>
2024-09-12 17:55:35 +0200

    corect php file

--- teachers.php
+++ teachers.php
@@ -9,17 +9,7 @@
  */
 
 /* `school-management`.`teachers` */
-// $teachers = array(
-//   array('full_name' => 'Jelena P','email' => 'jelena@skolers.org','password' => '$2y$10$Rnjfbh2GrB9Hl6ZYZtFfgOL7OB3RwlS9CqQWmAfsm8z5C8RpeY3nK'),
-//   array('full_name' => 'Saška Vilić','email' => 'saska.vilic@gmail.com','password' => '$2y$10$I40YIWHd3.EG1boFadjanOUhtiBk5ZoYX81F.hM3yqVbw7Fg.xcDW'),
-//   array('full_name' => 'Dusan Ivkovic','email' => 'dusan@skolers.org','password' => '$2y$10$oaB.dqK1T00frKBcliv03ex0NKcosjPaA.Vj2iF6jqcard/X9ortW'),
-//   array('full_name' => 'Adrijana Sarcevic','email' => 'adrijana@skolers.org','password' => '$2y$10$hycTEUxiZzglYQ/cXZLv1emRGs7KtVWerCyD5y8ViJZMpvGciKdei'),
-//   array('full_name' => 'Наташа Звјерац','email' => 'natasa@skolers.org','password' => '$2y$10$wZb9lSZxc3D7T9.7hAPLcuI0cpbkLQwuwgT3jvQg6UUGFFWb6ZN2y'),
-//   array('full_name' => 'Деспот Субић','email' => 'despot@skolers.org','password' => '$2y$10$Mrpecn8Jum0uQRFYYgtBO.TS7JdZMkEhM/cVWJqFz9sbzeHcmAizK'),
-//   array('full_name' => 'Зорана Миљковић','email' => 'zorana@skolers.org','password' => '$2y$10$WXfddQd06C6axlAiptO0buBfhUbdQAg0yN6Zxs26z3NjLyokHppO6'),
-//   array('full_name' => 'Јанко Веселиновић','email' => 'janko@skolers.com','password' => '$2y$10$7z2ahwQxbPXl7l9L6S77T.325cDqPecUKYLRFBfO1y.OCLn2J/ODC'),
-//   array('full_name' => 'Душан Ивковић','email' => 'dusan.ivkovic@skolers.org','password' => '$2y$10$ANxeDFgN7OlqZwjdQFfn4eelvA997c2QFhdsN0TFLailEvhn89SQ.')
-// );
+
 
 $newArr =  [
     0 => [
